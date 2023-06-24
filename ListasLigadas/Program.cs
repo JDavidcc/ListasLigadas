b71cc40477d1bb94bdb971fb8747d85ad9459408
David cervantes <xRaptor_XD@hotmail.com>
2023-06-23 20:58:35 -0700

    Agregar archivos de proyecto.

---
+++ ListasLigadas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CListaligada miLista = new CListaligada();
+
+            miLista.Adicionar(3);
+            miLista.Adicionar(4);
+            miLista.Adicionar(2);
+            miLista.Adicionar(1);
+
+            miLista.Transversa();
+        }
+    }
+}
